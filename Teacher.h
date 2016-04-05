de45c2b42df66dc22ec7229270df5ed3bdb83939
CD11 <cheryl.dunn40@gmail.com>
2016-04-04 23:37:10 -0400

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -8,6 +8,11 @@
 using namespace std;
 
 #include "defs.h"
+#include "User.h"
+#include "Book.h"
+#include "Course.h"
+#include "Product.h"
+#include "Dlist.h"
 
 /*	Purpose:
 	This is a derived class for teachers that can use the bookstore management system
@@ -15,26 +20,30 @@ using namespace std;
 	Teacher has polymorphic methods: view, addProduct, removeProduct, addCourse, removeCourse (from the main list)
 */
 
-class Teacher {
+class Teacher :public User
+{
 
   public:
     Teacher(int=0, double=0.00);//id, discountAmnt (in decimal)
 	virtual ~Teacher();
-    int 	getId();
-         void addProduct(string, Book*);
-     void removeProduct(string, string);
-     void addCourse(Course*);
-     void removeCourse(string);
-     void searchProduct(string);//search for course by course code or textbook by isbn
-
-    double    	getDiscount();
-
+    int 	getCode();
+	double  getDiscount();
+    void	addBook(Course*, Book*);
+    void 	add(Course*);
+	void	addMany(Dlist<Course*>&);
+	void 	remove(Course*);
+	void	removeMany(Dlist<Course*>&);
+   // virtual void 	searchProduct(string);//search for course by course code or textbook by isbn
+    void 	addProduct(Product*);
+    void 	removeProduct(Product*);
+	virtual Dlist<Product*>& getList();
+	virtual void searchProduct(string, Product*);
+	virtual void printList(Dlist<Product*>&);
+	virtual string getProduct();
   private:
     int   id;
     double      discountAmnt;
-        Dlist<Product*>*    courses;
-
+    Dlist<Course*>*    courses;
 };
 
 #endif
-
