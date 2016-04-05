8de65707cb6683a1761636123b1b8eb162031905
CD11 <cheryl.dunn40@gmail.com>
2016-04-04 23:25:58 -0400

    Update BMScontrol.cc

--- BMScontrol.cc
+++ BMScontrol.cc
@@ -5,10 +5,9 @@ using namespace std;
 
 BMScontrol::BMScontrol()
 {
-  courses = new Dlist<Course*>();
-  products = new Dlist<Product*>();
-  cart = new Dlist<Product*>();
-  view    = new UImanager;
+
+	users = new Dlist<User*>();
+  	view    = new UImanager;
 }
 
 BMScontrol::~BMScontrol(){}
@@ -16,194 +15,217 @@ BMScontrol::~BMScontrol(){}
 void BMScontrol::launch()
 {
 	int    choice, option;
-	string code, user;
+	string code;
 	
 	while (1) {
 		choice = -1;
 		option = -1;
-			/******  Main Menu ******/
-   		view->mainMenu(&choice);  
-   		if (choice == 0) { // clean up 
-			products->cleanupProducts();
-			cart->cleanupProducts();
-			courses->cleanup();
-
-			delete courses;
-			delete products;
-			delete cart;
+		/******  Main Menu ******/
+		view->mainMenu(&choice);  
+		User* user;
+		view->login(&user, &choice);
+		*users += user;
+	   	if (choice == 0) { // clean up 
+			user->getList().cleanupProducts();
+			//delete  *user->getList();
+			delete users;
 			delete view;		
 			break;
-    	}
+		}
 		/* loads employee menu*/
-    	else if (choice == 1) {  
-			while(option != 0){
+		else if (choice == 1) {
+	  			
+			while(1){
 				view->employeeMenu(&option);
 				if( option == 0){
+					break;
 				}
 				else if( option == 1){ // add book
 					Product* newProduct;
 					view->getBookData(&newProduct);
-					*products += newProduct;
+					 user->getList() += newProduct;
 				}
-				else if( option == 2){ // remove book ***** 
+				else if( option == 2){ // remove book
 					Product* newProduct;
-					view->getProduct(&newProduct, *products);
-					*products -= newProduct;
+					view->getProduct(&newProduct, user->getList());
+					 user->getList() -= newProduct;
 				}
 				else if(option == 3) { // add game
 					Product* newProduct;
 					view->getGameData(&newProduct);
-					*products += newProduct;
+					user->getList()+= newProduct;
 				}
-				else if( option == 4){ // remove game *********
+				else if( option == 4){ // remove game
 					Product* newProduct;
-					view->getProduct(&newProduct,  *products);
-					*products -= newProduct;
-	
+					view->getProduct(&newProduct,  user->getList());
+					 user->getList() -= newProduct;
 				}
 				else if( option == 5){ // add eReader 
 					Product* newProduct;
 					view->geteReaderData(&newProduct);
-					*products += newProduct;
+					user->getList() += newProduct;
 				}
-				else if( option == 6){ // remove eReader *****
+				else if( option == 6){ // remove eReader
 					Product* newProduct;
-					view->getProduct(&newProduct, *products);
-					*products -= newProduct;
+					view->getProduct(&newProduct, user->getList());
+					user->getList() -= newProduct;
 				}
 				else if( option == 7){ // add Writing
 					Product* newProduct;
 					view->getWritingData(&newProduct);
-					*products += newProduct;
+					user->getList() += newProduct;
 				}
-				else if( option == 8){ // remove Writing ******
+				else if( option == 8){ // remove Writing
 					Product* newProduct;
-					view->getProduct(&newProduct, *products);
-					*products -= newProduct;
+					view->getProduct(&newProduct, user->getList());
+					user->getList() -= newProduct;
 				}
 				else if(option == 9){ // print products
-					view->printProducts(*products);
+					view->printProducts(user->getList());
 				}
-			}	
+				view->pause();
+			}
   		}
-		/* loads customer menu */
-		else if (choice == 2) { 
-			while(option != 0){
-				view->customerMenu(&option);
+			/* loads customer menu */
+			else if (choice == 2) { 
+				while(1){	
+					view->customerMenu(&option);
 					if( option == 0){
+						break;
+					}
+					else if( option == 1){ // add book
+						Product* newProduct;
+						view->getBookData(&newProduct);
+						user->getList() += newProduct;
+					}
+					else if( option == 2){ // remove book
+						Product* newProduct;
+						view->getProduct(&newProduct, user->getList());
+						user->getList() -=newProduct;
+					}
+					else if(option == 3) { // add game
+						Product* newProduct;
+						view->getGameData(&newProduct);
+						user->getList() += newProduct;
+					}
+					else if( option == 4){ // remove game
+						Product* newProduct;
+						view->getProduct(&newProduct, user->getList());
+						user->getList() -= newProduct;
+					}
+					else if( option == 5){ // add eReader 
+						Product* newProduct;
+						view->geteReaderData(&newProduct);
+						user->getList() += newProduct;
+					}
+					else if( option == 6){ // remove eReader
+						Product* newProduct;
+						view->getProduct(&newProduct, user->getList());
+						user->getList() -= newProduct;
+					}
+					else if( option == 7){ // add Writing
+						Product* newProduct;
+						view->getWritingData(&newProduct);
+						user->getList() += newProduct;
+					}
+					else if( option == 8){ // remove Writing
+						Product* newProduct;
+						view->getProduct(&newProduct, user->getList());
+						user->getList() -= newProduct;
+					}
+					else if(option == 9){ // print products
+						view->printProducts(user->getList());
+					}
+					view->pause();
 				}
-				else if( option == 1){ // add book
-					Product* newProduct;
-					view->getBookData(&newProduct);
-					*cart += newProduct;
+	  		}
+			/* loads company menu*/
+			else if (choice == 3) { 
+				while(1){
+					view->companyMenu(&option);
+					if( option == 0){
+						break;
+					}
+					else if( option == 1){ // add product ++++
+						Product* newProduct;
+						string type = user->getProduct();
+						if (type == "Book"){
+							view->getBookData(&newProduct);
+						}
+						else if (type == "Game"){
+							view->getGameData(&newProduct);
+						}
+						else if (type == "eReader"){
+							view->geteReaderData(&newProduct);
+						}
+						else if (type == "Writing"){
+							view->getWritingData(&newProduct);
+						}
+						user->getList() += newProduct;
+					}
+					else if(option == 2) { // remove product   ******* 
+						Product* newProduct;
+						string type = user->getProduct();
+						view->getProduct(&newProduct, user->getList());
+						user->getList() -= newProduct;
+					}
+					else if(option == 3) { // change price 
+					
+						//user->getList() += newProduct;
+					
+					}
+					else if(option == 4){  // print products  *** EDIT SO ONLY PRINTS 1 TYPE of PRODUCT
+						view->printProducts(user->getList());
+					}
+				view->pause();
 				}
-				else if( option == 2){ // remove book ***** 
-					Product* newProduct;
-					view->getBookData(&newProduct);
-					*cart -=newProduct;
-				}
-				else if(option == 3) { // add game
-					Product* newProduct;
-					view->getGameData(&newProduct);
-					*cart += newProduct;
-				}
-				else if( option == 4){ // remove game *********
-					Product* newProduct;
-					view->getBookData(&newProduct);
-					*cart -= newProduct;
-				}
-				else if( option == 5){ // add eReader 
-					Product* newProduct;
-					view->geteReaderData(&newProduct);
-					*cart += newProduct;
-				}
-				else if( option == 6){ // remove eReader *****
-					Product* newProduct;
-					view->getBookData(&newProduct);
-					*cart -= newProduct;
-				}
-				else if( option == 7){ // add Writing
-					Product* newProduct;
-					view->getWritingData(&newProduct);
-					*cart += newProduct;
-				}
-				else if( option == 8){ // remove Writing ******
-					Product* newProduct;
-					view->getBookData(&newProduct);
-					*cart -= newProduct;
-				}
-				else if(option == 9){ // print products
-					view->printProducts(*cart);
-				}
-			}
-  		}
-		/* loads company menu*/
-		else if (choice == 3) { 
-			while(option != 0){
-				view->companyMenu(&option);
-				if( option == 0){
-				}
-				else if( option == 1){ // add product ++++
-					//Product* newProduct;
-					//view->getProductData(&newProduct);
-					//products[numProducts++] = newProduct;
-				}
-				else if(option == 2) { // remove product   ******* 
-	
-				}
-				else if(option == 3) { // change price 
-				}
-				else if(option == 4){  // print products  *** EDIT SO ONLY PRINTS 1 TYPE of PRODUCT
-					//view->printProducts(products, numProducts);
-				}
-			}
-    	}
-		/* loads teacher menu*/
-		else if (choice == 4) { 
-			while(option != 0){
-				view->teacherMenu(&option);
-				if( option == 0){
-				}
-				else if( option == 1){ // add single course
-					Course* newCourse;
-			  		view->getCourseData(&newCourse);
-					*courses += newCourse;
-				}
-				else if(option == 2) { // remove single course
-				
-					Course* newCourse;
-			 	 	view->removeCourseData(&newCourse, *courses);
-					*courses -= newCourse;
-				}
-				else if (option == 3) {// add multiple courses
-					Dlist<Course*> list;
-					view->getCourses(list);
-					view->printCourses(list);
-					*courses += list;
-				}
-				else if (option == 4) {//remove multiple courses
-					Dlist<Course*> list;     		
-					view->removeCourses(list, *courses);
-					*courses -= list;
-	  			}
-				else if (choice == 5) { // print courses
-					//view->printCourses(*courses);
-				}
-		   		else if (choice == 6){ // add Book  
-					Book* newBook; 
-					Product* newProduct;
-					view->getBook(&newBook, *courses);
-					//products[numProducts++] = newBook;
-					newProduct = newBook;
-					*products += newProduct;
-		   		}
-				else if (choice == 7) { // print books 
-					view->printBooks(*courses);
-				}
-			}
-		 view->pause();
-		}		
 
-  view->pause();
-  }
+			}
+			/* loads teacher menu*/
+			else if (choice == 4) {
+				while(1){
+					view->teacherMenu(&option);
+					if( option == 0){
+						break;
+					}
+					else if( option == 1){ // add single course
+						Course* newCourse;
+				  		view->getCourseData(&newCourse);
+					//	user->add(newCourse);
+					}
+					else if(option == 2) { // remove single course
+				
+						Course* newCourse;
+				 	 //	view->removeCourseData(&newCourse, user->getList());
+					//	*courses -= newCourse;
+					}
+					else if (option == 3) {// add multiple courses
+						Dlist<Course*> list;
+						view->getCourses(list);
+						view->printCourses(list);
+						//*courses += list;
+					}
+					else if (option == 4) {//remove multiple courses
+						Dlist<Course*> list;     		
+					//	view->removeCourses(list, user->getList());
+					//	*courses -= list;
+		  			}
+					else if (choice == 5) { // print courses
+					//	view->printCourses(*courses);
+					}
+			   		else if (choice == 6){ // add Book  
+						Book* newBook; 
+						Product* newProduct;
+					//	view->getBook(&newBook, user->getList());
+						newProduct = newBook;
+						user->getList() += newProduct;
+			   		}
+					else if (choice == 7) { // print books 
+					//	view->printBooks(user->getList());
+					}
+				  view->pause();
+				}
+			 view->pause();
+			}
+		}	
 }
