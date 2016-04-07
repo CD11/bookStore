aca112165395a2fdc1fa078937c6efacfdf148f0
Krithika Saravanan <KrithikaSaravanan@users.noreply.github.com>
2016-04-07 19:44:37 -0400

    Update Employee.cc

--- Employee.cc
+++ Employee.cc
@@ -14,22 +14,6 @@ Employee::~Employee()
 
 double Employee::getDiscount()    { return discountAmnt; }
 int    Employee::getCode()   { return id; }// called getCode so it can be used to sort in the Dlist class
-void Employee::addProduct(Product* item)
-{
-		*products += item;
-
-}
-
-void Employee::removeProduct(Product* item)
-{
-		*products -= item;
-
-}
-
-void Employee::searchProduct(string name, Product* p)
-{
-	p = *products->findObj(name);
-}
 
 void Employee::printList(Dlist<Product*>& p){
 	string s = "" ;
@@ -37,8 +21,3 @@ void Employee::printList(Dlist<Product*>& p){
 	p.printProducts(s, p);
     cout << s << endl<<endl;
 }
-
-Dlist<Product*>& Employee::getList(){
-	return *products;
-}
-
