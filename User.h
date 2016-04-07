45527d6d98fa8463b616c8234c52052713bdde09
Krithika Saravanan <KrithikaSaravanan@users.noreply.github.com>
2016-04-07 19:43:04 -0400

    Update User.h

--- User.h
+++ User.h
@@ -6,25 +6,27 @@
 #include <string>
 #include "Dlist.h"
 #include "defs.h"
+#include "Product.h"
+
 using namespace std;
 
 
-/*	Purpose:
-	This is a base class for all users that can use the bookstore management system
-	All users have a discount percent and an id. 
-	User has polymorphic methods: addProduct, removeProduct 
+/*  Purpose:
+    This is a base class for all users that can use the bookstore management system
+    All users have a discount percent and an id. 
+    User has polymorphic methods: addProduct, removeProduct 
 */
 
 class User {
 
   public:
     User(int=0, double=0.00);//id, discountAmnt (in decimal)
-	virtual ~User();
-    int 	getId();
+    virtual ~User();
+    int     getId();
     virtual void addProduct(Product*);
     virtual void removeProduct(Product*);
-    double    	getDiscount();
-    virtual Product* searchProduct(string) = 0;
+    double      getDiscount();
+    virtual void searchProduct(string, Product*);
 
   private:
     int   id;
